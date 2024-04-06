8fa9be1f225d0cab5a93d9a906ab38697bc49524
Sangita Dutta <sdutta06@students.cs.ubc.ca>
2024-04-06 13:52:03 -0700

    set up page routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Switch, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Community from './pages/CommunityPage';
+
+
 
 function App() {
   return (
-    <div className="App">
-    hello world
-    </div>
+    <Router>
+    <Routes> 
+      <Route path="/" element={<HomePage />} /> 
+      <Route path="/community" element={<Community />} /> 
+    </Routes>
+  </Router>
   );
 }
 
---
+++ src/pages/CommunityPage.jsx
@@ -0,0 +1,8 @@
+export default function Community() {
+    return (
+      <div className="App">
+        <h1>Community Page</h1>
+        <p>This is the community page where users can interact.</p>
+      </div>
+    );
+  }
