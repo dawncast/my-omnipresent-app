bbcfd4885e8e2f9bb56789e0ce37310c25166c1d
riyaazad <riyaazad@hotmail.com>
2024-04-06 21:29:53 -0700

    finished community cards

--- frontend/src/components/communities.jsx
+++ frontend/src/components/communities.jsx
@@ -2,58 +2,58 @@ import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/20/solid'
 
 const people = [
   {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    role: 'Admin',
-    email: 'janecooper@example.com',
+    name: 'Bouldering Meetup',
+    title: 'Location: The Hive Bouldering Gym',
+    role: '2 days away',
+    email: '',
     telephone: '+1-202-555-0170',
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+      'https://particle.scitech.org.au/wp-content/uploads/2019/11/bouldering_feature.jpg',
   },
   {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    role: 'Admin',
+    name: 'Ski and Snowboard club',
+    title: 'Location: Cypress Mountain',
+    role: '2 days away',
     email: 'janecooper@example.com',
     telephone: '+1-202-555-0170',
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+      'https://www.switchbacktravel.com/sites/default/files/articles%20/Snowboarding%20in%20powder%20%28women%27s%20snowboard%20pants%20m%29.jpg',
   },
   {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    role: 'Admin',
+    name: 'Trek around Grouse Mountain',
+    title: 'Location: Grouse Mountain',
+    role: '4 days away',
     email: 'janecooper@example.com',
     telephone: '+1-202-555-0170',
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+      'https://bearfoottheory.com/wp-content/uploads/2020/09/Montana_Whitefish-Ski-Resort-22-Hiking-Gear-oboz.jpg',
   },
   {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    role: 'Admin',
+    name: 'Cycling Adventure',
+    title: 'Location: Yaletown SeaWall',
+    role: '6 days away',
     email: 'janecooper@example.com',
     telephone: '+1-202-555-0170',
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+      'https://hips.hearstapps.com/hmg-prod/images/cyclist-on-the-col-de-la-colombiere-in-the-french-royalty-free-image-1650363392.jpg?crop=0.604xw:0.882xh;0.111xw,0.103xh&resize=640:*',
   },
   {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    role: 'Admin',
+    name: 'Women\'s Exclusive Hike',
+    title: 'Location: Panorama Ridge',
+    role: '9 days away',
     email: 'janecooper@example.com',
     telephone: '+1-202-555-0170',
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+      'https://www.getoutsideadventures.ca/images/headers/GetOutside-Backpacking-Instructing-Guide-Mountains-Women-lg.jpg',
   },
   {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    role: 'Admin',
+    name: 'Nature ',
+    title: 'Location: Stanley Park',
+    role: '14 days away',
     email: 'janecooper@example.com',
     telephone: '+1-202-555-0170',
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+      'https://www.antiloneliness.com/uploads/4/8/3/7/48374419/published/untitled-design-2.jpg?1624538993',
   },
 
   // More people...
@@ -83,8 +83,12 @@ export default function Communities() {
                   href={`mailto:${person.email}`}
                   className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                 >
-                  <EnvelopeIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                  Email
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+</svg>
+
+                  RSVP
                 </a>
               </div>
               <div className="-ml-px flex w-0 flex-1">
@@ -92,8 +96,11 @@ export default function Communities() {
                   href={`tel:${person.telephone}`}
                   className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                 >
-                  <PhoneIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                  Call
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+</svg>
+
+                  Details
                 </a>
               </div>
             </div>
@@ -103,3 +110,5 @@ export default function Communities() {
     </ul>
   )
 }
+
+
